e9840ca60e2433563e3c32ea7057fbc561816436
paulo_dell <aldazpaulo@gmail.com>
2024-12-06 11:45:32 -0500

    creacion de roles y configs de application.yml

--- iam/src/main/java/com/labo/iam/repo/model/Permission.java
+++ iam/src/main/java/com/labo/iam/repo/model/Permission.java
@@ -1,25 +1,19 @@
 package com.labo.iam.repo.model;
 
-
 import java.time.LocalDateTime;
-import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
-@Table(name = "roles")
+@Table(name = "permissions")
 @Data
-public class Role {
+public class Permission {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -32,13 +26,4 @@ public class Role {
 
     @Column(name = "created_at")
     private LocalDateTime createdAt = LocalDateTime.now();
-
-    @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(
-        name = "role_permissions",
-        joinColumns = @JoinColumn(name = "role_id"),
-        inverseJoinColumns = @JoinColumn(name = "permission_id")
-    )
-    private Set<Permission> permissions;
-
 }
--- iam/src/main/java/com/labo/iam/security/JwtAuthenticationEntryPoint.java
+++ iam/src/main/java/com/labo/iam/security/JwtAuthenticationEntryPoint.java
@@ -1,14 +1,17 @@
 package com.labo.iam.security;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.AuthenticationEntryPoint;
-
 import java.io.IOException;
 import java.io.Serializable;
 
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Serializable {
     
     @Override
